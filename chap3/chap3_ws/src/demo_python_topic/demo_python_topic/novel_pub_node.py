ef0658cedecc1ec4a0e4eb9092c91e27564023bb
Moco818 <xie_f@buaa.edu.cn>
2025-09-10 18:12:13 +0800

    build(chap2): 更新 demo_cpp_pkg 并添加新功能
    - 移除 .gitignore 中的 .vscode 目录，保留 browse.vc.db 文件
    - 在 demo_cpp_pkg 中添加 learn_thread 可执行文件
    - 包含项目头文件目录以支持后续功能开发

---
+++ chap3/chap3_ws/src/demo_python_topic/demo_python_topic/novel_pub_node.py
@@ -0,0 +1,34 @@
+import rclpy
+from rclpy.node import Node
+import requests
+from example_interfaces.msg import String
+from queue import Queue
+
+class NovelPubNode(Node):
+    def __init__(self, node_name: str, *, context: rclpy.Context | None = None, cli_args: rclpy.List[str] | None = None, namespace: str | None = None, use_global_arguments: bool = True, enable_rosout: bool = True, start_parameter_services: bool = True, parameter_overrides: rclpy.List[rclpy.Parameter] | None = None, allow_undeclared_parameters: bool = False, automatically_declare_parameters_from_overrides: bool = False) -> None:
+        super().__init__(node_name, context=context, cli_args=cli_args, namespace=namespace, use_global_arguments=use_global_arguments, enable_rosout=enable_rosout, start_parameter_services=start_parameter_services, parameter_overrides=parameter_overrides, allow_undeclared_parameters=allow_undeclared_parameters, automatically_declare_parameters_from_overrides=automatically_declare_parameters_from_overrides)
+        self.novels_queue_ = Queue()
+
+        self.novels_publisher_ = self.create_publisher(String, 'novel', 10)
+        self.timer_ = self.create_timer(5, self.timer_callback)
+
+    def download_novel(self, url):
+        response = requests.get(url)
+        response.encoding = 'utf-8'
+        self.get_logger().info(f'Download Finshed: {url}')
+        for line in response.text.splitlines():
+            self.novels_queue_.put(line)
+
+    def timer_callback(self):
+        if self.novels_queue_.qsize() > 0:
+            msg = String()
+            msg.data = self.novels_queue_.get()
+            self.novels_publisher_.publish(msg)
+            self.get_logger().info(f'Novel: {msg.data}')
+
+def main():
+    rclpy.init()
+    node = NovelPubNode('novel_pub')
+    node.download_novel('http://66666666')
+    rclpy.spin(node)
+    rclpy.shutdown()
---
+++ chap3/chap3_ws/src/demo_python_topic/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'demo_python_topic'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='moco',
+    maintainer_email='xie_f@buaa.edu.cn',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            "novel_pub_node = demo_python_topic.novel_pub_node:main"
+
+        ],
+    },
+)

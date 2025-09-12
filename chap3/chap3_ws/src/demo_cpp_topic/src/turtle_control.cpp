d1794cb89c611225f62d985f2def2960a1fba651
Moco818 <xie_f@buaa.edu.cn>
2025-09-12 18:50:25 +0800

    feat(chap3): 添加海龟控制节点并优化代码
    - 新增 turtle_control 可执行文件
    - 更新 CMakeLists.txt，添加新节点的编译和安装
    - 修改 turtle_control.cpp，实现基本的海龟控制逻辑
    - 移除 c_cpp_properties.json 中的冗余包含路径

--- chap3/chap3_ws/src/demo_cpp_topic/src/turtle_control.cpp
+++ chap3/chap3_ws/src/demo_cpp_topic/src/turtle_control.cpp
@@ -26,8 +26,36 @@ private:
          * 
          * 该函数在接收到乌龟的位置信息后被调用，用于计算位置误差并发布相应的速度控制指令
          */
-        void on_pose_received_(const turtlesim::msg::Pose::SharedPtr pose) {
+        void on_pose_received_(const turtlesim::msg::Pose::SharedPtr pose) 
+        {
         	// TODO: 收到位置计算误差，发布速度指令
+            auto message = geometry_msgs::msg::Twist();
+            // 1. 记录当前位置
+            double current_x = pose -> x;
+            double current_y = pose -> y;
+            RCLCPP_INFO(this -> get_logger(), "当前位置：（x=%f, y=%f）", current_x, current_y);
+            // 2. 计算与目标之间的距离，以及与当前海龟朝向的角度差
+            double distance = std::sqrt((target_x_ - current_x) * (target_x_ - current_x) + (target_y_ - current_y) * (target_y_ - current_y));
+            double angle = std::atan2(target_y_ - current_y, target_x_ - current_y) - pose -> theta;
+            // 3. 控制策略：距离大于 0.1 继续运动，角度差大于 0.2 则原地旋转，否则直行
+            if (distance > 0.1)
+            {
+                if (fabs(angle) > 0.2)
+                {
+                    message.angular.z = fabs(angle);
+                }
+                else
+                {
+                    message.linear.x = k_ * distance;
+                }
+                
+            };
+            // 4. 限制最大值并发布消息
+            if (message.linear.x > max_speed_)
+            {
+                message.linear.x = max_speed_;
+            }
+            velocity_publisher_ -> publish(message);
         }
 
 private:
